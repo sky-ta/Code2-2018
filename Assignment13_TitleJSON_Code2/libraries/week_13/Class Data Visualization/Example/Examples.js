eb2723cbfdab341a3bdc913690881e5ae52df3c8
Skyler Ta <tat491@newschool.edu>
2018-05-01 12:37:31 -0400

    rerereupload

---
+++ Assignment13_TitleJSON_Code2/libraries/week_13/Class Data Visualization/Example/Examples.js
@@ -0,0 +1,32 @@
+//Bellow is Rian's EXAMPLE
+		// processData(){
+		// 		console.log(this.table.getColumn('What is your favorite ice cream flavor?'));
+		// 		let answers = this.table.getColumn('What is your favorite ice cream flavor?');
+		// 		this.flavors =  [];
+		// 		this.icecream = [];
+		// 		for(var i =0; i<14;i++){
+		// 			this.flavors.push(answers[i]);
+		// 			this.icecream.push(new IceCream(this.flavors[i], this.tileSize));
+		// 		}
+		// 	}
+	// Bellow is Jonathan's EXAMPLE (only work with binary0:
+		// this.lonelyYes = 0;
+		// this.lonelyNo = 0;
+
+		// this.lonelyAnswers = this.table.getColumn('How do you like your fries?');
+		// console.log(this.lonelyAnswers);
+
+
+		// for (let i = 0; i < this.lonelyAnswers; i++){
+		// 	if (this.lonelyAnswers[i]==='Yes'){
+		// 		this.lonelyYes++;
+		// // 	}else{
+		// // 		this.lonelyNo++;
+		// 	}
+		// }
+
+		// console.log(this.lonelyYes);
+	// End of EXAMPLE.
+
+	
+xc
