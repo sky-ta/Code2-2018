d4926887b2725e6d2ebe907a4a7839fa6760acff
Skyler Ta <tat491@newschool.edu>
2018-05-08 23:14:54 -0400

    new info

--- MIDTERM_p5.js_PlayAnimation_Code2/sketch.js
+++ MIDTERM_p5.js_PlayAnimation_Code2/sketch.js
@@ -2,31 +2,122 @@
 
 //animations like p5 images should be stored in variables
 //in order to be displayed during the draw cycle
-var ghost;
-// asterisk;
+var begula;
+var crab;
+var octopus;
+var starfish;
+var ray;
 
-//it's advisable (but not necessary) to load the images in the preload function
-//of your sketch otherwise they may appear with a little delay
-function preload() {
-
-//create an animation from a sequence of numbered images
-//pass the first and the last file name and it will try to find the ones in between
-ghost = loadAnimation("assets/16-A03-1.png", "assets/16-A03-4.png");
-  
-//create an animation listing all the images files
-// asterisk = loadAnimation("assets/asterisk.png", "assets/triangle.png", "assets/square.png", "assets/cloud.png", "assets/star.png", "assets/mess.png", "assets/monster.png");
-}
 
 function setup() {
-  createCanvas(800,300);
+  createCanvas(900, 500);  
+
+  // document.bgColor="#CCCCCC"
+  //BEGULA:
+  begula = createSprite(220, 100);
+  begula.addAnimation("normal","assets/begula01.png", "assets/begula03.png");
+  begula.addAnimation("annoyed","assets/begula-angry01.png", "assets/begula-angry03.png");
+
+  begula.mouseActive = true;
+  begula.setCollider("circle", 0,0,64);
+
+  //CRAB:
+  crab = createSprite(600, 200);
+  crab.addAnimation("normal","assets/crab01.png", "assets/crab03.png");
+  crab.addAnimation("annoyed","assets/crab-angry01.png", "assets/crab-angry03.png");
+
+  crab.mouseActive = true;
+  crab.setCollider("circle", 0,0,64);
+	
+  //OCTOPUS:
+  octopus = createSprite(710, 150);
+  octopus.addAnimation("normal","assets/octopus01.png", "assets/octopus03.png");
+  octopus.addAnimation("annoyed","assets/octopus-angry01.png", "assets/octopus-angry03.png");
+
+  octopus.mouseActive = true;
+  octopus.setCollider("circle", 0,0,80);
+
+  //STARFISH:
+  starfish = createSprite(400, 150);
+  starfish.addAnimation("normal","assets/starfishs01.png", "assets/starfish03.png");
+  starfish.addAnimation("annoyed","assets/starfish-angry01.png", "assets/starfish-angry03.png");
+
+  starfish.mouseActive = true;
+  starfish.setCollider("circle", 0,0,80);
+
+  //RAY:
+  ray = createSprite(450, 250);
+  ray.addAnimation("normal","assets/ray01.png", "assets/ray03.png");
+  ray.addAnimation("annoyed","assets/ray-angry01.png", "assets/ray-angry03.png");
+
+  ray.mouseActive = true;
+  ray.setCollider("circle", 0,0,69);
+
+
+  //FUNCTIONS FOR ANIMATION:
+  begula.onMouseOver = function() {
+  this.changeAnimation("annoyed");
+  }
+  begula.onMouseOut = function() {
+  this.changeAnimation("normal");
+  }
+	
+	crab.onMouseOver = function() {
+  this.changeAnimation("annoyed");
+  }
+  crab.onMouseOut = function() {
+  this.changeAnimation("normal");
+  }  
+
+  octopus.onMouseOver = function() {
+  this.changeAnimation("annoyed");
+  }
+  octopus.onMouseOut = function() {
+  this.changeAnimation("normal");
+  }
+
+  starfish.onMouseOver = function() {
+  this.changeAnimation("annoyed");
+  }
+  starfish.onMouseOut = function() {
+  this.changeAnimation("normal");
+  }  
+
+  ray.onMouseOver = function() {
+  this.changeAnimation("annoyed");
+  }
+  ray.onMouseOut = function() {
+  this.changeAnimation("normal");
+  }  
+  
+  // begula.onMousePressed = function() {
+  // this.changeAnimation("annoyed");
+  // this.animation.goToFrame(this.animation.getLastFrame());
+  // }
+  
+  // begula.onMouseReleased = function() {
+  // this.changeAnimation("annoyed");
+  // this.animation.goToFrame(0);
+  // }
+  
 }
 
 function draw() {
   background(255,255,255);  
+  if(begula.mouseIsOver)
+      begula.rotation-= 10;
+  // begula.visible = !begula.mouseIsPressed;
   
-  //specify the animation instance and its x,y position
-  //animation() will update the animation frame as well
-  animation(ghost, 300, 150);
-  // animation(asterisk, 500, 150);
-}
+  if(crab.mouseIsOver)
+      crab.rotation-= 10;
 
+  if(octopus.mouseIsOver)
+      octopus.rotation-= 10;
+
+  if(starfish.mouseIsOver)
+      starfish.rotation-= 10;
+
+  if(ray.mouseIsOver)
+      ray.rotation-= 10;
+  drawSprites();
+}
